8fc9ac1bfdf36470c25b0dd4675c882cd85d439d
Ramanantsoa <rtnaty@me.com>
2020-10-31 12:04:09 -0400

    portfolio

---
+++ precache-manifest.6e387a7fa26149e5c1c818591799a1ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a81f97285ae5e5f39e4578913f9ccf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c8ce5176d55d8e5735a",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "9018ff592dea7c89616b",
+    "url": "/static/js/2.f851fbc0.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "/static/js/2.f851fbc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c8ce5176d55d8e5735a",
+    "url": "/static/js/main.470131cc.chunk.js"
+  },
+  {
+    "revision": "8dfdd9c9f417f1f5fa77",
+    "url": "/static/js/runtime-main.33f2df4b.js"
+  }
+]);
