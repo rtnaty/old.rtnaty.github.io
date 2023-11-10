056e0cd69fd588cbfb631f14c11f2a499356d3e9
Mamitina <tsilavina.ramanatsoa@techinnov.ca>
2023-11-10 15:16:39 -0500

    commit build project

---
+++ precache-manifest.691c9edc487ed7cbf24338a23ae59e93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "892ca6c39fea49c71acb58601eb33532",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8371c3ca788c178771e",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "de61a3cf6114222da2fd",
+    "url": "/static/js/2.b2ef2d38.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "/static/js/2.b2ef2d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8371c3ca788c178771e",
+    "url": "/static/js/main.5b034e0b.chunk.js"
+  },
+  {
+    "revision": "8dfdd9c9f417f1f5fa77",
+    "url": "/static/js/runtime-main.33f2df4b.js"
+  }
+]);
